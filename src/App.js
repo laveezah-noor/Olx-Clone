ce63c5aab572a60cdede8354bad071c67ee67c9c
unknown <laveezahnoor@gmail.com>
2020-10-24 00:23:19 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -2,23 +2,40 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function Header() {
+  return (
+    <div>
+      <h1>Header</h1>
+    </div>
+  )
+
+}
+
+function Main() {
+  return (
+    <div>
+      <h1>Header</h1>
+    </div>
+  )
+
+}
+
+function Footer() {
+  return (
+    <div>
+      <h1>Header</h1>
+    </div>
+  )
+
+}
+
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Main />
+      <Footer />
     </div>
   );
 }
